cccea3edc8606881c29a6cf9e791c729dcfe099a
G384552023 <134909576+G384552023@users.noreply.github.com>
2025-05-23 17:55:41 +0900

    index.htmlを新規作成

--- practice-dom2/dom2-exercise.js
+++ practice-dom2/dom2-exercise.js
@@ -1,8 +1,42 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>URL</key>
-	<string>https://www.nishita-lab.org/web-contents/text_current/practice-dom2/dom2-exercise.js</string>
-</dict>
-</plist>
+//////////////// ここは書き換えてはいけない！ 
+let campus = {
+	address: "八王子市館町",
+	buildingD: ["D101", "D102", "D103", "D201", "D202", "D203", "D204", "D205"],
+	lon: 35.624869704425,
+	lat: 139.28201056633
+};
+
+let gakka = [
+	{name: "機械システム工学科", ename: "Department of Mechanical Systems Engineering"},
+	{name: "電子システム工学科", ename: "Department of Electronics and Computer Systems"},
+	{name: "情報工学科", ename: "Department of Computer Science"},
+	{name: "デザイン学科", ename: "Department of Design"}
+];
+
+//////////////// ここから下にプログラムを書きたそう!
+let p=document.querySelector('button#show');
+p.addEventListener('click',greeting);
+function greeting() {
+	console.log(campus.address);
+    let a =document.createElement('ul');
+    a.textContent=campus.address;
+	let b=document.querySelector('h2#addr');
+	b.insertAdjacentElement('beforeend',a);
+for(let gakka1 of gakka) {
+	console.log(gakka1.name);
+    let l =document.createElement('li');
+    l.textContent=gakka1.name;
+	let u=document.querySelector('h2#dept');
+	u.insertAdjacentElement('beforeend',l);
+}
+}
+
+console.log(campus.address);
+
+for (let gakka1 of gakka) {
+	console.log(gakka1.name);
+  }
+
+	
+
+  
